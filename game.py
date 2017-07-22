63b1d24d44876e89660357986db110cc94110797
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-07-22 16:53:11 -0400

    0.0.1 initial commit

---
+++ game.py
@@ -0,0 +1,16 @@
+#project folder name : pygame
+#file name : game
+#Author : Chinatsu Kawakami
+#Description: this is my first pygame project
+#Date 22 July 2017
+#version : 0.0.1
+
+import pygame
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
+
+crashed = False
+
