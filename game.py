a31c3884db6f6ae7dba61f44b64248a253f9a5e2
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-07-22 17:06:35 -0400

    0.0.2 add the code to show user's mouse position or which key user pushed

--- game.py
+++ game.py
@@ -14,3 +14,14 @@ clock = pygame.time.Clock()
 
 crashed = False
 
+while not crashed:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashd = True
+    print(event)
+
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
+quit()
